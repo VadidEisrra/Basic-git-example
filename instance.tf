f74153d8eaf922b934fe7a4943ef37a18415133b
vadid.eisrra <david.sierra@packetfabric.com>
2022-08-23 14:50:02 -0400

    Formatting & consolidation of security groups

--- instance.tf
+++ instance.tf
@@ -19,17 +19,18 @@ resource "aws_instance" "web_server" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public[0].id
   key_name                    = "main"
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id, aws_security_group.allow_web.id]
+  vpc_security_group_ids      = [aws_security_group.allow_web_ssh.id]
 
   user_data = file("user-data-apache.sh")
+
   tags = {
     Name = "${var.env_code}-web-server"
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_web_ssh" {
+  name        = "allow_web_ssh"
+  description = "Allow web and ssh inbound traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -38,19 +39,6 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["66.30.140.145/32"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 80
