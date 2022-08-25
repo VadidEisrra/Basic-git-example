269ca5ee15719b61d2beda095f699a08c53328d6
vadid.eisrra <david.sierra@packetfabric.com>
2022-08-25 10:04:34 -0400

    Changes: - instance.tf specify ec2 ami name and reduce resource footprint - user-data-apache.sh add steps to clone and deploy project

--- user-data-apache.sh
+++ user-data-apache.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 yum update -y
+yum install -y git
 yum install -y httpd
+cd /var/www
+git clone https://github.com/gabrielecirulli/2048.git
+sed -i 's+/var/www/html+/var/www/2048+g' /etc/httpd/conf/httpd.conf
 systemctl start httpd && systemctl enable httpd
